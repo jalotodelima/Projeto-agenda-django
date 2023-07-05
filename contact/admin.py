23c76fa71dd24163fac17e27358fa32d584b0265
Igor Jaloto <jalotodelima@gmail.com>
2023-07-05 16:53:23 -0300

    Aula models

--- contact/admin.py
+++ contact/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from contact.models import Contact
+from contact import models
 
 # Register your models here.
 
 
-@admin.register(Contact)
+@admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone')
+    ordering = '-id',  # ordenação descrescente.. por isso o menos na frente
+    # list_filter = 'created date' # filtrar ao lado
+    search_fields = 'id', 'first_name', 'last_name'
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'phone', 'last_name'
+    list_display_links = 'first_name', 'id'
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = '-id',  # ordenação descrescente.. por isso o menos na frente
--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,17 @@ from django.utils import timezone
 # picture (imagem)
 
 
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
@@ -16,7 +27,16 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
+    category = models.ForeignKey(
+        Category,
+        # quando apagar a categoria, fica nulo no nome da categoria no contato
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     # colocar o nome lá na base de dados (antes ficaria "contact object (1 ou ID que fosse)")
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
