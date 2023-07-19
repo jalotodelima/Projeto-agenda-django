5dc67783c71c9acddb602b00261bbcf27d8d5db6
Igor Jaloto <jalotodelima@gmail.com>
2023-07-19 18:26:21 -0300

    forms django

---
+++ contact/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from . import models
+
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = models.Contact
+        fields = ('first_name', 'last_name', 'phone')
+
+    def clean(self):
+        # cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Error.. tu és buro',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,22 @@
-from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, redirect
-from contact.models import Contact
-from django.db.models import Q
+from django.shortcuts import render
+from contact.forms import ContactForm
 
 # Create your views here.
 
 
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
 
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
     context = {
-
+        'form': ContactForm()
     }
 
     return render(
