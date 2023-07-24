d7be10ec216a1c3b881c113d444201b159bff9a8
Igor Jaloto <jalotodelima@gmail.com>
2023-07-24 16:58:59 -0300

    login e logout

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from . import models
 
 
@@ -51,3 +52,39 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    email = forms.EmailField(
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    # Validar email
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        # se existir algum usuário com esse email
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail'),
+                code='invalid',
+            )
+        return email
